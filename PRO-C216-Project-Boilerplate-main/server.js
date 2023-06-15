cb36ce3ad14aab298aa691509d8d386b02ee7bf4
VAIBHAV JAIN <64785307+vibhu0209@users.noreply.github.com>
2023-06-15 15:48:49 +0530

    Update server.js

--- PRO-C216-Project-Boilerplate-main/server.js
+++ PRO-C216-Project-Boilerplate-main/server.js
@@ -4,6 +4,12 @@ const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+const io = require("socket.io")(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
 app.get("/", (req, res) => {
     res.render("index");
 });
